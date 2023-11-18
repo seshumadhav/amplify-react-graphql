5fd00178979850a6ae1d155526d83af01e4e096d
Seshumadhav Chaturvedula Venkata <seshumadhav@gmail.com>
2023-11-18 17:11:52 -0600

    Updated with dummy react code to show a richer UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [jobId, setJobId] = useState('');
+  const [jobseekerIds, setJobseekerIds] = useState('');
+  const [jobInfo, setJobInfo] = useState({
+    requirements: 'Experience in React, Node.js',
+    salary: '$70,000',
+    location: 'Remote',
+    perks: 'Healthcare, Flexible Hours',
+  });
+  const [jobSeekerInfo, setJobSeekerInfo] = useState({
+    qualifications: "Bachelor's in Computer Science",
+    preferences: 'Remote work, Full-time',
+    occupations: 'Frontend Developer',
+  });
+  const [scoresAndAttributes, setScoresAndAttributes] = useState({
+    momScore: 80,
+    dadScore: 75,
+    overlappingAttributes: ['React', 'Remote'],
+  });
+
+  const handleJobIdChange = (e) => setJobId(e.target.value);
+  const handleJobseekerIdsChange = (e) => setJobseekerIds(e.target.value);
+
+  const handleSubmit = () => {
+    // Placeholder for data retrieval logic
+    // Replace this with actual data fetching code
+    // For now, let's keep using the dummy data
+  };
+
+  const handleCancel = () => {
+    // Reset the form
+    setJobId('');
+    setJobseekerIds('');
+    setJobInfo({
+      requirements: '',
+      salary: '',
+      location: '',
+      perks: '',
+    });
+    setJobSeekerInfo({
+      qualifications: '',
+      preferences: '',
+      occupations: '',
+    });
+    setScoresAndAttributes({
+      momScore: 0,
+      dadScore: 0,
+      overlappingAttributes: [],
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Input Section */}
+      <div>
+        <label>
+          Job ID:
+          <input type="text" value={jobId} onChange={handleJobIdChange} />
+        </label>
+        <label>
+          Jobseeker IDs:
+          <input type="text" value={jobseekerIds} onChange={handleJobseekerIdsChange} />
+        </label>
+        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </div>
+
+      {/* Output Section */}
+      <div>
+        {/* Panel 1: Job Information */}
+        <div>
+          <h2>Job Information</h2>
+          <p>Requirements: {jobInfo.requirements}</p>
+          <p>Salary: {jobInfo.salary}</p>
+          <p>Location: {jobInfo.location}</p>
+          <p>Perks: {jobInfo.perks}</p>
+        </div>
+
+        {/* Panel 2: Job Seeker Information */}
+        <div>
+          <h2>Job Seeker Information</h2>
+          <p>Qualifications: {jobSeekerInfo.qualifications}</p>
+          <p>Preferences: {jobSeekerInfo.preferences}</p>
+          <p>Occupations: {jobSeekerInfo.occupations}</p>
+        </div>
+
+        {/* Panel 3: Scores and Attributes */}
+        <div>
+          <h2>Scores and Attributes</h2>
+          <p>MOM Score: {scoresAndAttributes.momScore}</p>
+          <p>DAD Score: {scoresAndAttributes.dadScore}</p>
+          <p>Overlapping Attributes: {scoresAndAttributes.overlappingAttributes.join(', ')}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
