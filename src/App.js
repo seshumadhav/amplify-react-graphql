8abd93e28340d475a3a3a3ed8d5e4d0e73158c9b
Seshumadhav Chaturvedula Venkata <seshumadhav@gmail.com>
2023-11-18 17:51:12 -0600

    Stylize the UI

--- src/App.js
+++ src/App.js
@@ -57,14 +57,26 @@ const App = () => {
     <div className="container mt-4 app-container">
       {/* Input Section */}
       <div className="mb-4">
-        <label className="form-label">
-          Job ID:
-          <input type="text" className="form-control" value={jobId} onChange={handleJobIdChange} />
-        </label>
-        <label className="form-label">
-          Jobseeker IDs:
-          <input type="text" className="form-control" value={jobseekerIds} onChange={handleJobseekerIdsChange} />
-        </label>
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <label className="form-label">
+              Job ID:
+              <input type="text" className="form-control" value={jobId} onChange={handleJobIdChange} />
+            </label>
+          </div>
+          <div className="col-md-6 mb-3">
+            <label className="form-label">
+              Jobseeker IDs:
+              <input
+                type="text"
+                className="form-control"
+                value={jobseekerIds}
+                onChange={handleJobseekerIdsChange}
+              />
+            </label>
+          </div>
+        </div>
+        <hr className="my-4" /> {/* Horizontal line to separate Input and Output sections */}
         <button className="btn btn-primary me-2" onClick={handleSubmit}>
           Submit
         </button>
@@ -74,52 +86,56 @@ const App = () => {
       </div>
 
       {/* Output Section */}
-      <div>
+      <div className="d-flex flex-wrap">
         {/* Panel 1: Job Information */}
-        <div className="border p-4 mb-4">
-          <h2 className="text-muted">Job Information</h2>
-          <p>
-            <strong>Requirements:</strong> {jobInfo.requirements}
-          </p>
-          <p>
-            <strong>Salary:</strong> {jobInfo.salary}
-          </p>
-          <p>
-            <strong>Location:</strong> {jobInfo.location}
-          </p>
-          <p>
-            <strong>Perks:</strong> {jobInfo.perks}
-          </p>
+        <div className="col-md-6 mb-4 pe-md-3">
+          <div className="border p-4 h-100 d-flex flex-column">
+            <h2 className="text-muted">Job Information</h2>
+            <p>
+              <strong>Requirements:</strong> {jobInfo.requirements}
+            </p>
+            <p>
+              <strong>Salary:</strong> {jobInfo.salary}
+            </p>
+            <p>
+              <strong>Location:</strong> {jobInfo.location}
+            </p>
+            <p>
+              <strong>Perks:</strong> {jobInfo.perks}
+            </p>
+          </div>
         </div>
 
         {/* Panel 2: Job Seeker Information */}
-        <div className="border p-4 mb-4">
-          <h2 className="text-muted">Job Seeker Information</h2>
-          <p>
-            <strong>Qualifications:</strong> {jobSeekerInfo.qualifications}
-          </p>
-          <p>
-            <strong>Preferences:</strong> {jobSeekerInfo.preferences}
-          </p>
-          <p>
-            <strong>Occupations:</strong> {jobSeekerInfo.occupations}
-          </p>
+        <div className="col-md-6 mb-4 ps-md-3">
+          <div className="border p-4 h-100 d-flex flex-column">
+            <h2 className="text-muted">Job Seeker Info</h2>
+            <p>
+              <strong>Qualifications:</strong> {jobSeekerInfo.qualifications}
+            </p>
+            <p>
+              <strong>Preferences:</strong> {jobSeekerInfo.preferences}
+            </p>
+            <p>
+              <strong>Occupations:</strong> {jobSeekerInfo.occupations}
+            </p>
+          </div>
         </div>
+      </div>
 
-        {/* Panel 3: Scores and Attributes */}
-        <div className="border p-4">
-          <h2 className="text-muted">Scores and Attributes</h2>
-          <p>
-            <strong>MOM Score:</strong> {scoresAndAttributes.momScore}
-          </p>
-          <p>
-            <strong>DAD Score:</strong> {scoresAndAttributes.dadScore}
-          </p>
-          <p>
-            <strong>Overlapping Attributes:</strong>{' '}
-            {scoresAndAttributes.overlappingAttributes.join(', ')}
-          </p>
-        </div>
+      {/* Panel 3: Overlap Info & Scores*/}
+      <div className="border p-4">
+        <h2 className="text-muted">Overlap Info & Scores</h2>
+        <p>
+          <strong>MOM Score:</strong> {scoresAndAttributes.momScore}
+        </p>
+        <p>
+          <strong>DAD Score:</strong> {scoresAndAttributes.dadScore}
+        </p>
+        <p>
+          <strong>Overlapping Attributes:</strong>{' '}
+          {scoresAndAttributes.overlappingAttributes.join(', ')}
+        </p>
       </div>
     </div>
   );
