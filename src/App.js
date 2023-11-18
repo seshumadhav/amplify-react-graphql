15e031580db8fe379a46a82da6745285fb8b82ae
Seshumadhav Chaturvedula Venkata <seshumadhav@gmail.com>
2023-11-18 17:30:19 -0600

    Update code to have better font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './App.css'; // Import custom CSS for font styling
 
 const App = () => {
   const [jobId, setJobId] = useState('');
@@ -53,7 +54,7 @@ const App = () => {
   };
 
   return (
-    <div className="container mt-4">
+    <div className="container mt-4 app-container">
       {/* Input Section */}
       <div className="mb-4">
         <label className="form-label">
@@ -64,35 +65,60 @@ const App = () => {
           Jobseeker IDs:
           <input type="text" className="form-control" value={jobseekerIds} onChange={handleJobseekerIdsChange} />
         </label>
-        <button className="btn btn-primary me-2" onClick={handleSubmit}>Submit</button>
-        <button className="btn btn-secondary" onClick={handleCancel}>Cancel</button>
+        <button className="btn btn-primary me-2" onClick={handleSubmit}>
+          Submit
+        </button>
+        <button className="btn btn-secondary" onClick={handleCancel}>
+          Cancel
+        </button>
       </div>
 
       {/* Output Section */}
       <div>
         {/* Panel 1: Job Information */}
-        <div className="bg-light p-4 mb-4">
+        <div className="border p-4 mb-4">
           <h2 className="text-muted">Job Information</h2>
-          <p><strong>Requirements:</strong> {jobInfo.requirements}</p>
-          <p><strong>Salary:</strong> {jobInfo.salary}</p>
-          <p><strong>Location:</strong> {jobInfo.location}</p>
-          <p><strong>Perks:</strong> {jobInfo.perks}</p>
+          <p>
+            <strong>Requirements:</strong> {jobInfo.requirements}
+          </p>
+          <p>
+            <strong>Salary:</strong> {jobInfo.salary}
+          </p>
+          <p>
+            <strong>Location:</strong> {jobInfo.location}
+          </p>
+          <p>
+            <strong>Perks:</strong> {jobInfo.perks}
+          </p>
         </div>
 
         {/* Panel 2: Job Seeker Information */}
-        <div className="bg-light p-4 mb-4">
+        <div className="border p-4 mb-4">
           <h2 className="text-muted">Job Seeker Information</h2>
-          <p><strong>Qualifications:</strong> {jobSeekerInfo.qualifications}</p>
-          <p><strong>Preferences:</strong> {jobSeekerInfo.preferences}</p>
-          <p><strong>Occupations:</strong> {jobSeekerInfo.occupations}</p>
+          <p>
+            <strong>Qualifications:</strong> {jobSeekerInfo.qualifications}
+          </p>
+          <p>
+            <strong>Preferences:</strong> {jobSeekerInfo.preferences}
+          </p>
+          <p>
+            <strong>Occupations:</strong> {jobSeekerInfo.occupations}
+          </p>
         </div>
 
         {/* Panel 3: Scores and Attributes */}
-        <div className="bg-light p-4">
+        <div className="border p-4">
           <h2 className="text-muted">Scores and Attributes</h2>
-          <p><strong>MOM Score:</strong> {scoresAndAttributes.momScore}</p>
-          <p><strong>DAD Score:</strong> {scoresAndAttributes.dadScore}</p>
-          <p><strong>Overlapping Attributes:</strong> {scoresAndAttributes.overlappingAttributes.join(', ')}</p>
+          <p>
+            <strong>MOM Score:</strong> {scoresAndAttributes.momScore}
+          </p>
+          <p>
+            <strong>DAD Score:</strong> {scoresAndAttributes.dadScore}
+          </p>
+          <p>
+            <strong>Overlapping Attributes:</strong>{' '}
+            {scoresAndAttributes.overlappingAttributes.join(', ')}
+          </p>
         </div>
       </div>
     </div>
