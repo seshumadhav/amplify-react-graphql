ff66e62917159b29766ba4175abcfcdc70075815
seshumadhav <95457657+seshumadhav@users.noreply.github.com>
2023-11-18 16:50:08 -0600

    Update App.js with a personalized message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world! I am feeling welcomed to WS Amplify. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
